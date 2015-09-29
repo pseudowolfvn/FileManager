504c89cf248df9f83b9622eaf0c9c1f4b5262c26
Katrychuk Dmytro <pseudowolf.vn@gmail.com>
2015-09-29 21:26:25 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace FileManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
