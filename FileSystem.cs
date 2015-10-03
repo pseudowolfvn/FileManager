c118bf0e410b8a723412f50199bfc3a8f41ce865
Katrychuk Dmytro <pseudowolf.vn@gmail.com>
2015-10-03 19:24:11 +0300

    Initial base of classes

---
+++ FileSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Entities
+{
+    static class FileSystem
+    {
+        static DriveInfo[] allDrives = DriveInfo.GetDrives();
+        public static int GetNumOfDrives()
+        {
+            return allDrives.Length;
+        }
+        public static DriveInfo[] GetDrives()
+        {
+            return allDrives;
+        }
+    }
+}
---
+++ Panel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Entities
+{
+    class Panel
+    {
+        static int currentDrive;
+        DriveInfo activeDrive;
+        DirectoryInfo currentDirectory;
+        public Panel()
+        {
+            activeDrive = FileSystem.GetDrives()[(currentDrive++) % FileSystem.GetNumOfDrives()];
+            currentDirectory = activeDrive.RootDirectory;
+        }
+    }
+}
