778f232945b397ba97b8dc938bfefcb3dd3ea70b
Katrychuk Dmytro <pseudowolf.vn@gmail.com>
2015-10-20 11:13:50 +0300

    Added UI for additional (previous commit) text-viewing functionality

--- TextViewer.xaml.cs
+++ TextViewer.xaml.cs
@@ -20,25 +20,33 @@ namespace FileManager
     /// </summary>
     public partial class TextViewer : Window
     {
+        static bool redundantWhiteSpaces = true;
+        string document;
         FileInfo file;
         public TextViewer(FileInfo file)
         {
             this.file = file;
             InitializeComponent();
+            SetDefaultDocument();
             View();
+        }
+
+        private void SetDefaultDocument()
+        {
+            document = File.ReadAllText(this.file.FullName, Encoding.Default);
         }
 
         private void View()
         {
             Paragraph text = new Paragraph();
-            text.Inlines.Add(File.ReadAllText(this.file.FullName, Encoding.Default));
+            text.Inlines.Add(document);
             FlowDocument content = new FlowDocument(text);
             flowDoc.Document = content;
         }
 
         public static List<List<string>> LongestWords(string source, int n)
         {
-            string[] words = source.Split(' ');
+            string[] words = source.Split(' ', '\t', '\r', '\n');
             List<List<string>> result = new List<List<string>>(n);
             for (int i = 0; i < n; i++)
             {
@@ -46,26 +54,27 @@ namespace FileManager
                 result[i].Add("");
             }
             foreach (var word in words)
-            {
-                int i = 0;
-                while (i < n && word.Length < result[i][0].Length) ++i;
-                if (i != n)
+                if (word.Length != 0)
                 {
-                    if (word.Length == result[i][0].Length) result[i].Add(word);
-                    else
+                    int i = 0;
+                    while (i < n && word.Length < result[i][0].Length) ++i;
+                    if (i != n)
                     {
-                        for (int index = n - 1; index > i; --index)
+                        if (word.Length == result[i][0].Length) result[i].Add(word);
+                        else
                         {
-                            List<string> copy = new List<string>(result[index - 1]);
-                            result[index].Clear();
-                            result[index] = copy;
+                            for (int index = n - 1; index > i; --index)
+                            {
+                                List<string> copy = new List<string>(result[index - 1]);
+                                result[index].Clear();
+                                result[index] = copy;
+                            }
+                            result[i].Clear();
+                            result[i].Add(word);
                         }
-                        result[i].Clear();
-                        result[i].Add(word);
-                    }
 
+                    }
                 }
-            }
             return result;
         }
         private enum States { Default, WhiteSpaces, NewLine };
@@ -104,6 +113,39 @@ namespace FileManager
 
             }
             return result;
+        }
+
+        private void ShowTenLongestWords(object sender, RoutedEventArgs e)
+        {
+            var result = LongestWords(document, 10);
+            string info = "Ten longest words in this document: \n";
+            foreach (var words in result)
+                if (words[0].Length != 0)
+                {
+                    info += "Length " + words[0].Length + ": ";
+                    foreach (var word in words)
+                    {
+                        info += word + "\t ";
+                    }
+                    info += "\n";
+                }
+            MessageBoxResult message = MessageBox.Show(info);
+        }
+
+        private void RemoveWhitespaces(object sender, RoutedEventArgs e)
+        {
+            if (redundantWhiteSpaces == true)
+            {
+                redundantWhiteSpaces = false;
+                document = RemoveWhitespaces(document);
+                View();
+            }
+            else
+            {
+                redundantWhiteSpaces = true;
+                SetDefaultDocument();
+                View();
+            }
         }
     }
 }
