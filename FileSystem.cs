c5a792c7273db73eaf2a8e298ab637b50b279d0a
Katrychuk Dmytro <pseudowolf.vn@gmail.com>
2015-10-27 02:02:54 +0200

    Merged two versions together (MultiPanel with huge fix pack)

--- FileSystem.cs
+++ FileSystem.cs
@@ -10,15 +10,20 @@ namespace FileManager.Entities
 {
     static class FileSystem
     {
-        static DriveInfo[] allDrives = DriveInfo.GetDrives();
+        static List<DriveInfo> allDrives;
+        static FileSystem()
+        {
+            allDrives = new List<DriveInfo>();
+            foreach (var x in DriveInfo.GetDrives())
+                if (x.IsReady) allDrives.Add(x);
+        }
         public static int GetNumOfDrives()
         {
-            return allDrives.Length;
+            return allDrives.Count;
         }
-
         public static DriveInfo[] GetAllDrives()
         {
-            return allDrives;
+            return allDrives.ToArray();
         }
 
         public static DriveInfo GetDrive(string name)
